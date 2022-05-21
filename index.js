26cd1812ee75d9e9ee16b46051a4334d6aba02a8
Dominique Kornegay <dominiquekornegay@gmail.com>
2022-05-21 12:08:05 -0400

    commit tree ba5a347e779d0837e821245f245d3c1cb18a26dc

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(namesArray, suprise) {
+    let message = []
+    for (let i = 0; i < namesArray.length; i++) {
+        message.push(`Thank you, ${namesArray[i]}, for the wonderful ${suprise} gift!`)
+    }
+ return message;
+}
+
+function countDown() {
+    let countDown = 10;
+        while (countDown > -1) {
+            console.log(countDown--);
+        }
+    return countDown;
+}
